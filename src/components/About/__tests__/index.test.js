9e946d2ebfa1e2a187c4e9ecf84db25917798684
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-04-09 18:46:56 -0500

    nav test

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render, cleanup} from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-afterEach(cleanup) // afterEach is a global function from Jest
+// afterEach is a global function from Jest
+// This will ensure that after each test, we won't have any
+// leftover memory data that could give us false results.
+afterEach(cleanup)
 
 describe('About component', () => {
     // first test
