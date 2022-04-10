d4803c1d5160aa2dfba9f658ac224c89f0af9cea
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-04-10 10:53:28 -0500

    display all photos in the gallary

--- src/App.js
+++ src/App.js
@@ -21,10 +21,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
